9d4a291ef463e75eda5452de136ca962a7cee3ce
Henokaa <69524541+Henokaa@users.noreply.github.com>
2020-12-19 12:20:45 +0300

    implementing Youtube
    working with Youtube API

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
+
 import YouTube from "react-youtube";
 var getYouTubeID = require("get-youtube-id");
 
 export default function Note() {
+const [id, setid] = useState(" ");
+
+function handleChange(e) {
+  setid(getYouTubeID(e.target.value))
+}
+
+const opts = {
+  height: '500',
+  width: '830',
+  playerVars: {
+    // https://developers.google.com/youtube/player_parameters
+    autoplay: 0,
+  },
+};
 
     return (
-        <div>
-          hello  
+        <div className="APP">
+          <input type="text" onChange={handleChange} required placeholder="URL..." />
+          <YouTube videoId={id} opts={opts} />
         </div>
     )
 }
