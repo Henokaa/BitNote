fc1cc061c4911259d334e6217c7aac2cf686cb93
Henokaa <69524541+Henokaa@users.noreply.github.com>
2020-12-20 17:47:32 +0300

    testing Youtube title

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -6,11 +6,15 @@ var getYoutubeTitle = require('get-youtube-title');
 
 export default function Note() {
 const [id, setid] = useState();
+const [ytitle, setytitle] =  useState();
 
 function handleChange(e) {
   setid(getYouTubeID(e.target.value));
+ // console.log(getYouTubeID(e.target.value));
+   getYoutubeTitle(getYouTubeID(e.target.value), function (err, title) {
+    setytitle(title); // 'SLCHLD - EMOTIONS (feat. RIPELY) ()
+   })
 }
-
 
 const opts = {
   height: '500',
@@ -30,6 +34,7 @@ const _onVeady = (event) => {
         <div className="APP">
           <input type="text" onChange={handleChange} required placeholder="URL..." />
           <YouTube videoId={id} opts={opts} onReady={_onVeady}/>
+          <h1>h{ytitle}</h1>
         </div>
     )
     
